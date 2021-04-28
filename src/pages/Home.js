8f7a5bd46763218d048b5eff7598089acd941aa7
Flavien BERNARD <flavien.bernard09@gmail.com>
2021-04-28 19:45:01 +0400

    Remove unused code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import Footer from '../components/Footer/Footer';
-import NavBar from '../components/NavBar/NavBar';
 
 const Home = function Home() {
     return (
         < div >
-            <div><NavBar></NavBar></div>
-
-            Hello from home
-
-            <div><Footer></Footer></div>
+            Hello from home           
         </div >
-
-
     );
 }
 
