a58e69eaee6a199f274ae6c65962b68d5f6e8e5a
Flavien BERNARD <flavien.bernard09@gmail.com>
2021-04-29 08:25:23 +0400

    NavBar style and dev

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,14 @@
 import { React } from "react";
+import { Link } from "react-router-dom";
 
 const NavBar =  () => {
     return (
 
         <div className="NavBar">
-            <p> Mon NavBar </p>
+            <h1>Flavien BERNARD</h1>
+            <Link>About</Link>
+            <Link>Projects</Link>
+            <Link>Contact</Link>
         </div>
 
     );
